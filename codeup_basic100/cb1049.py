acd0521ac3a48572c18828a5afe2fe683d133077
yuri_lee <leeyuri.andapp@gmail.com>
2021-01-26 22:22:07 +0900

    Update cb1049.py

--- codeup_basic100/cb1049.py
+++ codeup_basic100/cb1049.py
@@ -3,8 +3,8 @@ a, b = map(int, input().split())
 print(1) if a>b else print(0)
 
 //다른 풀이
-/*
-a,b=map(int, input().split())
+# 
+# a,b=map(int, input().split())
 
-print(int(a>b))
-*/
+# print(int(a>b))
+# 
