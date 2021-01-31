17c8d2848b69759234185f972ec2726bd2d8ad98
yuri_lee <leeyuri.andapp@gmail.com>
2021-01-31 21:35:52 +0900

    Update greedy_q1.py

--- this_is_codingtest_with_python/Greedy/greedy_q1.py
+++ this_is_codingtest_with_python/Greedy/greedy_q1.py
@@ -2,7 +2,7 @@ data = input() # 여러개의 숫자로 구성된 문자열
 result = int(data[0])
 
 for i in range(1, len(data)):
-  if (result<=1):
+  if (result<=1 or int(data[i])<=1):
     result+=int(data[i])
   else:
     result*=int(data[i])
