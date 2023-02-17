3a2aa274a8df60f6e23e989837c18371b01d75e4
Lee Yuri <37764504+yurrrri@users.noreply.github.com>
2023-02-17 13:37:08 +0000

    백준 2470 - 투포인터

---
+++ Baekjoon/2470.py
@@ -0,0 +1,30 @@
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+arr = list(map(int, sys.stdin.readline().rstrip().split()))
+arr.sort()
+
+left = 0 #시작점
+right = n-1 #끝점
+answer = abs(arr[left] + arr[right]) #초기 절대값
+
+a = 0
+b = 0
+
+while left < right: #두개가 겹치면 안되므로
+    left_val = arr[left]
+    right_val = arr[right]
+
+    sum = left_val + right_val
+  
+    if abs(sum) <= answer:
+        answer = abs(sum)
+        a = arr[left]
+        b = arr[right]
+
+    if sum < 0: 
+      left += 1
+    else:
+      right -= 1
+    
+print(a, b)      
